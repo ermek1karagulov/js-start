c13fa6e3cd40887dbdeb01bb7c15ba299c185fbf
ermekkaragulov0@gmail.com <ermek1karagulov>
2022-11-15 13:26:25 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// console.log(arr[6]);
+// console.log(arr[7]);
+// console.log(arr[8]);
+
+//for---------------------------
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+let str = [1, 2, 3, 4, 5];
+for (let i = 0; i < str.length; i++) {
+  console.log(str[i] * 2);
+}
